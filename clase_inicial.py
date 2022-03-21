4c4d54ab82e33a1e3deb81c5771e4d8ef1384848
OctavioRey <81595160+OctavioRey@users.noreply.github.com>
2022-03-21 19:15:29 -0300

    Add files via upload

---
+++ clase_inicial.py
@@ -0,0 +1,61 @@
+import random
+
+
+#n1 = 2
+#n2 = 10
+
+#print (n1 ** n2)
+
+
+#if (n2 > 0):
+#    print('es positivo xd')
+#elif(n2 < 0):
+#    print ('es negativo xD')
+#else:
+#    print('es neutrovich')
+
+# Ciclos while - for
+
+
+#while (n1 < n2):
+#    print('valor: ',n1)
+#    n1 += 1
+
+# Operadores logicos AND OR NOT `
+
+# Estructura de datos array ---> Listas
+
+
+vec = [4, 3, 5, 8, 0, 15]
+
+
+
+print(type(vec), len(vec))
+
+
+
+#for i in range (0, len(vec)):
+#    print(vec[i])
+
+#for elemento in vec:
+#    print(elemento)
+
+#vec.reverse()
+
+#for elemento in vec:
+#    print(elemento)
+
+v1 = 3
+v2 = 5
+
+def mi_funcion(valor1, valor2, valor3=None, *otros):
+    print('lo que haga mi funcion xD')
+    print ('lista argumentos')
+    for elemento in otros:
+        print(elemento)
+
+print(vec)
+mi_funcion(v1,v2)
+
+print(v1,v2, 76, 15, 20, 35)
+
